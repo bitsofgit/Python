65576dafb7208eae1d6098a02c133992ab81531e
Akhil Deshpande <deshpandeakhil@gmail.com>
2017-10-08 12:09:55 -0400

    functions

--- HelloWorld.py
+++ HelloWorld.py
@@ -60,8 +60,81 @@ if num==3 and numstr=="123":
 
 if num==3 or numstr=="123sdfas":
     print("still good")
-    
+
+#ternary    
 print("big" if num > 2 else "small")
+
+print("-------Lists----------")
+names = ["Roger", "Rafael", "John", "Stan", "Andy"]
+print(names[0])
+print(names[-1]) #last item in the list
+names.append("Pete")
+
+if "Pete" in names:
+    print("Pete is in there")
+
+print(len(names))
+
+del names[4]
+print(names)
+
+print("----------Loop------------")
+for n in names:
+    print(n)
+
+for i in range(0,len(names),1):
+    print(names[i])
+
+for n in names:
+    if n == "John":
+        print("Found him")
+        break
+
+for n in names:
+    if n == "Stan":
+        continue
+    print(n) 
+
+i = 0
+while i < 10:
+    print(i)
+    i = i + 1
+
+print("----------Dictionary------------")
+players = [{ "Name" : "Rafael Nadal", "Country" : "Spain"},
+{ "Name" : "Roger Federer", "Country" : "Switzerland"},
+{ "Name" : "Pete Sampras", "Country" : "USA"}]
+
+print(players)
+def print_players():
+    for player in players:
+        str = player["Name"] + " " + player["Country"]
+        print(str)
+
+players.append({ "Name" : "Andre Agassi", "Country" : "USA"})
+print_players()
+
+for player in players:
+    if player["Name"] == "Andre Agassi":
+        players.remove(player)
+print_players()
+
+print("----------Exceptions------------")
+try:
+    #i = 5 + "abc"
+    i = 5 / 0
+except TypeError as error:
+    print("TypeError happened")
+    print(error)
+except Exception as error:
+    print(error)
+
+print("----------Functions------------")
+def add(i, j):
+    return i + j
+
+print(add(4,5))
+
 
 
 print("-----------------------")
